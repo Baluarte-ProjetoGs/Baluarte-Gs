2dfedcfd98e8b307776785c9faf157106fb50ec1
giiuk <giovannalb070607@gmail.com>
2025-05-30 21:44:39 -0300

    SlideShow

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+
+let slideIndex = 0;
+const slides = document.getElementsByClassName("slide");
+
+function mostrarSlide(index) {
+    if (index >= slides.length) slideIndex = 0;
+    if (index < 0) slideIndex = slides.length - 1;
+
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+
+    slides[slideIndex].style.display = "block";
+}
+
+function mudarSlide(n) {
+    slideIndex += n;
+    mostrarSlide(slideIndex);
+}
+
+
+function autoPlay() {
+    slideIndex++;
+    mostrarSlide(slideIndex);
+    setTimeout(autoPlay, 5000);
+}
+
+
+mostrarSlide(slideIndex);
+autoPlay()
